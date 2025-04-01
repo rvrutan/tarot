47b63dff1c75f9ebd3d655eba29b406ca14cddd8
rvrutan <rutanroni@gmail.com>
2025-03-31 22:13:08 -0400

    components files created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 // import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 // import 'm00.jpg' from '../../server/assets/cards'
 
 function App() {
