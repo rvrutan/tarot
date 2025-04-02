63cc4e90f66e4c90a08a0795b51c5985250747f5
Josh Kramer <kramer@nyu.edu>
2025-04-01 20:37:37 -0400

    added reversed logic

--- vite-project/src/components/BasicReading.jsx
+++ vite-project/src/components/BasicReading.jsx
@@ -4,16 +4,25 @@ import Card from './Card';
 const BasicReading = ({ cards }) => {
   const positions = ['Past', 'Present', 'Future'];
 
+  // Coin flip function to determine if the card is upright
+  const coinFlip = () => {
+    let headsOrTails = Math.round(Math.random());
+    return headsOrTails === 1; // Returns true for upright, false for reversed
+  };
+
   return (
     <div className="flex justify-around items-stretch my-5">
-      {cards.map((card, index) => (
-        <div key={index} className="flex-1 mx-2">
-          <h3 className="text-center text-lg font-semibold mb-2">
-            {positions[index]}
-          </h3>
-          <Card card={card} />
-        </div>
-      ))}
+      {cards.map((card, index) => {
+        const isUpright = coinFlip(); // Call coinFlip inside the map method
+        return (
+          <div key={index} className="flex-1 mx-2">
+            <h3 className="text-center text-lg font-semibold mb-2 text-black">
+              {positions[index]}
+            </h3>
+            <Card card={card} isUpright={isUpright} />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({ card }) => {
+// add props for reversed or upright
+// add a prop for
+
+const Card = ({ card, isUpright }) => {
   const imagePath = `${import.meta.env.BASE_URL}cards/${card.img}`;
+  const meaningIndex = Math.floor(
+    Math.random() * card.meanings[isUpright ? "light" : "shadow"].length
+  );
+  const meaning = card.meanings[isUpright ? "light" : "shadow"][meaningIndex];
 
   return (
     <div className="card shadow border border-gray-300 p-4 text-center rounded-lg text-black">
       {/* Card Name */}
       <h2 className="text-xl font-bold mb-2">{card.name}</h2>
+      <h2 className={`text-xl font-bold mb-2 ${!isUpright ? "" : "hidden"}`}>Reversed</h2>
+      {console.log(isUpright)}
 
       {/*Card Image*/}
+
       <img
         src={imagePath}
         alt={card.name}
-        className="w=full h-auto round-md mb-2"
+        className={`w-full h-auto rounded-md mb-2 transform ${
+          !isUpright ? "rotate-180" : ""
+        }`}
       />
 
       {/*Arcana and Suit side by side*/}
@@ -21,8 +33,8 @@ const Card = ({ card }) => {
         <span>{card.suit}</span>
       </div>
 
-      {/*Description using fortune_telling right now*/}
-      <p className="italic text-black">{card.fortune_telling[0]}</p>
+      {/*Description using Light = Upright, Dark = Reversed right now*/}
+      <p className="italic text-black">{meaning}</p>
     </div>
   );
 };
