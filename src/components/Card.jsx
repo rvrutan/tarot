d1a8f62dc55e7aa3bb174c1c71108c5f1af932a3
rvrutan <rutanroni@gmail.com>
2025-04-04 16:06:40 -0400

    card mobile responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,20 +24,20 @@ const Card = ({ card, isUpright }) => {
       <img
         src={`${import.meta.env.BASE_URL}cards/${card.img}`}
         alt={card.name}
-        className={`w-32 sm:w-40 h-auto rounded-md mb-2 transform ${
+        className={`w-20 sm:w-40 h-auto rounded-md mb-3 sm:mb-2 transform ${
           !isUpright ? "rotate-180" : ""
         }`}
       />
-      <h2 className="text-lg sm:text-xl font-bold mb-2">{card.name}</h2>
+      <h2 className="text-lg sm:text-xl font-bold mb-2 break-words">{card.name}</h2>
       <h2
-        className={`badge badge-soft badge-lg p-2 flex items-center justify-center rounded-sm mb-4 w-24 sm:w-32 ${
+        className={`badge badge-soft badge-lg p-2 flex items-center justify-center rounded-sm mb-3 sm:mb-4 w-24 sm:w-32 ${
           isUpright ? "invisible" : ""
         }`}
       >
         Reversed
       </h2>
       <div className="p-2">
-        <p className="italic text-sm sm:text-base">{meaning}</p>
+        <p className="italic text-sm sm:text-base break-words">{meaning}</p>
       </div>
     </div>
   );
